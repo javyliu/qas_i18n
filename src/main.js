a80459f61c41ab21861790c38065492fc5b10bbb
javy_liu <javy_liu@163.com>
2017-12-06 15:47:06 +0800

    添加yml-loader，用于配置多语言设置，到此，基本配置全部完成，开发多语言基于vue的语言设置完成

--- src/main.js
+++ src/main.js
@@ -4,28 +4,12 @@ import Vue from 'vue';
 import App from './App';
 import router from './router';
 import VueI18n from 'vue-i18n';
+import messages from './locales.yml';
 
 Vue.use(VueI18n);
 
 Vue.config.productionTip = false;
-const messages = {
-  'zh-CN': {
-    display_name: '中文',
-    hello: '世界，你好 {name}',
-    apple: '一个都没有 | 一个苹果 | {count} 个苹果'
-  },
-  en: {
-    display_name: '英文',
-    hello: 'hello world {name}',
-    apple: 'no apples | one apple | {count} apples'
-  },
-  ja: {
-    display_name: '日文',
-    hello: 'こんにちは、世界  {name}',
-    apple: 'no apples | one apple | {count} apples'
 
-  }
-};
 const i18n = new VueI18n({
   locale: navigator.language, // set locale
   fallbackLocale: 'en',

6e868c014f7490603bdc58aa6c51017ba054a473
javy_liu <javy_liu@163.com>
2017-12-06 11:59:46 +0800

    vue init webpack qas_i18n 初始化后， 安装 sass-loader, node-sass, foundation-sites， 并配置utils.js 来配scss来加入 foundation css“

---
+++ src/App.vue
@@ -0,0 +1,100 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+    <div class="grid-x">
+      <div class="cell">full width cell</div>
+      <div class="cell">full width cell</div>
+    </div>
+    <div class="grid-x">
+      <div class="small-6 cell">6 cells</div>
+      <div class="small-6 cell">6 cells</div>
+    </div>
+    <div class="grid-x">
+      <div class="medium-6 large-4 cell">12/6/4 cells</div>
+      <div class="medium-6 large-8 cell">12/6/8 cells</div>
+    </div>
+    <div class="grid-x ">
+      <div class="medium-6 large-4 cell">12/6/4 cells</div>
+      <div class="medium-6 large-8 cell">12/6/8 cells</div>
+    </div>
+    <div class="grid-x grid-padding-x">
+      <div class="medium-6 large-4 cell">12/6/4 cells</div>
+      <div class="medium-6 large-8 cell">12/6/8 cells</div>
+    </div>
+    <div class="grid-container">
+      <div class="grid-x grid-margin-x">
+        <div class="cell small-4">cesdfadfa l</div>
+        <div class="cell small-4">cell</div>
+        <div class="cell small-4">cell</div>
+      </div>
+    </div>
+    <div class="grid-container full">
+      <div class="grid-x grid-padding-x">
+        <div class="cell small-4">cadfasdasdf sadfasdfasdf sadfasdfasdf asdfasdfasdfell</div>
+        <div class="cell small-4">cell</div>
+        <div class="cell small-4">cell</div>
+      </div>
+    </div>
+    <div class="grid-container">
+      <div class="grid-x">
+        <div class="small-4 cell">4 cells</div>
+        <div class="auto cell">Whatever's left!</div>
+        <div class="auto cell">Whatever's leftWhatever's leftWhatever's leftWhatever's leftWhatever's leftWhatever's left!</div>
+      </div>
+    </div>
+    <div class="grid-x">
+      <div class="medium-auto cell">One</div>
+      <div class="medium-auto cell">Two</div>
+      <div class="medium-auto cell">Three</div>
+      <div class="medium-auto cell">Four</div>
+      <div class="medium-auto cell">Five</div>
+      <div class="medium-auto cell">Six</div>
+    </div>
+    <div class="grid-x grid-margin-x medium-margin-collapse">
+      <div class="small-6 cell">
+        Gutters at small no gutters at medium.
+      </div>
+      <div class="small-6 cell">
+        Gutters at small no gutters at medium.
+      </div>
+    </div>
+    <div class="grid-x grid-padding-x small-up-2 medium-up-4 large-up-6">
+      <div class="cell">cell</div>
+      <div class="cell">cell</div>
+      <div class="cell">cell</div>
+      <div class="cell">cell</div>
+      <div class="cell">cell</div>
+      <div class="cell">cell</div>
+    </div>
+    <div class="grid-y" style="height: 500px;">
+      <div class="cell small-6 medium-8 large-2">
+        6/8/2
+      </div>
+      <div class="cell small-6 medium-4 large-10">
+        6/4/10
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style lang="scss">
+@import "./styles/global";
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.grid-x div {
+  background: #ccc;
+}
+</style>

b18a0b17555d59e2aed54959e3eaa32ca13bbb48
javy_liu <javy_liu@163.com>
2017-12-13 16:41:40 +0800

    更新泰语翻译

--- config/prod.env.js
+++ config/prod.env.js
@@ -3,7 +3,7 @@ function get_api_url() {
   if (process.env.CLI_ENV === 'release') {
     return '"http://m.pipgame.com/api/v1"';
   }
-  return '"http://192.168.0.252:7092/api/v1"';
+  return '"http://192.168.0.252:7123/api/v1"';
 }
 module.exports = {
   NODE_ENV: '"production"',

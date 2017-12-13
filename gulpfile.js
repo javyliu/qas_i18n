243714ea380f889a9c53b255a48bf66732308715
javy_liu <javy_liu@163.com>
2017-12-13 18:19:02 +0800

    添加布署脚本

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+
+const gulp = require('gulp');
+const rsync = require('gulp-rsync');
+//布署到正式服
+gulp.task('deploy_to_web', function () {
+  return gulp.src('dist/**')
+    .pipe(rsync({
+      root: 'dist',
+      username: 'oswap',
+      progress: true,
+      hostname: 'pipgame.com',
+      destination: '/data3/web_apps/qas_i18n',
+      //destination: '../../web_apps/inner_app',
+      // exclude: ['*.swp'],
+      // chmod: "ugo=rwX",
+      incremental: true
+    }));
+});
+//布署到测试服
+gulp.task('deploy_to_252', function () {
+  return gulp.src('dist/**')
+    .pipe(rsync({
+      root: 'dist',
+      username: 'oswap',
+      progress: true,
+      hostname: '192.168.0.252',
+      destination: '/home/oswap/web_apps/qa_i18n',
+      //destination: '../../web_apps/inner_app',
+      // exclude: ['*.swp'],
+      // chmod: "ugo=rwX",
+      incremental: true
+    }));
+});
